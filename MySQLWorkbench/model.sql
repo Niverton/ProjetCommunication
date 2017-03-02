5c848e75a03d258d1110cd3eb9e8bd8d2c956bd6
Rémy M <niverton@live.fr>
2017-03-02 16:51:38 +0100

    Ajout description session dans modèle BDD

--- MySQLWorkbench/model.sql
+++ MySQLWorkbench/model.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2017-03-02 16:21
+-- Generated: 2017-03-02 16:50
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -9,51 +9,23 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-ALTER TABLE `mydb`.`sessions_has_oeuvres` 
-DROP FOREIGN KEY `fk_sessions_has_oeuvres_oeuvres1`;
+ALTER TABLE `mydb`.`oeuvres` 
+DROP FOREIGN KEY `fk_oeuvres_auteurs1`;
 
 ALTER TABLE `mydb`.`oeuvres` 
-DROP COLUMN `auteur`,
-CHANGE COLUMN `id` `id_oeuvre` INT(11) NOT NULL AUTO_INCREMENT ,
-CHANGE COLUMN `date` `date` VARCHAR(60) NULL DEFAULT NULL COMMENT 'c\'est un var char pour mettre n\'importe quel format (si oeuvre ancienne on peut se retrouver avec du 500AC)' ,
-ADD COLUMN `url_image` VARCHAR(1000) NULL DEFAULT NULL AFTER `description`,
-ADD COLUMN `auteurs_id_auteurs` INT(11) NOT NULL AFTER `url_image`,
-DROP PRIMARY KEY,
-ADD PRIMARY KEY (`id_oeuvre`, `auteurs_id_auteurs`),
-ADD INDEX `fk_oeuvres_auteurs1_idx` (`auteurs_id_auteurs` ASC);
+CHANGE COLUMN `url_image` `url_image` VARCHAR(2000) NULL DEFAULT NULL ,
+CHANGE COLUMN `auteurs_id_auteurs` `auteur_id_auteurs` INT(11) NOT NULL ;
 
 ALTER TABLE `mydb`.`sessions` 
-CHANGE COLUMN `id` `id_session` INT(11) NOT NULL AUTO_INCREMENT ;
+ADD COLUMN `description` LONGTEXT NULL DEFAULT NULL AFTER `date_fin`;
 
-ALTER TABLE `mydb`.`sessions_has_oeuvres` 
-CHANGE COLUMN `sessions_id` `id_sessions` INT(11) NOT NULL ,
-CHANGE COLUMN `oeuvres_id` `id_oeuvres` INT(11) NOT NULL ;
-
-CREATE TABLE IF NOT EXISTS `mydb`.`auteurs` (
-  `id_auteurs` INT(11) NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_auteurs`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ALTER TABLE `mydb`.`auteurs` 
+CHANGE COLUMN `id_auteurs` `id_auteur` INT(11) NOT NULL AUTO_INCREMENT ;
 
 ALTER TABLE `mydb`.`oeuvres` 
 ADD CONSTRAINT `fk_oeuvres_auteurs1`
-  FOREIGN KEY (`auteurs_id_auteurs`)
-  REFERENCES `mydb`.`auteurs` (`id_auteurs`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `mydb`.`sessions_has_oeuvres` 
-DROP FOREIGN KEY `fk_sessions_has_oeuvres_sessions`;
-
-ALTER TABLE `mydb`.`sessions_has_oeuvres` ADD CONSTRAINT `fk_sessions_has_oeuvres_sessions`
-  FOREIGN KEY (`id_sessions`)
-  REFERENCES `mydb`.`sessions` (`id_session`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_sessions_has_oeuvres_oeuvres1`
-  FOREIGN KEY (`id_oeuvres`)
-  REFERENCES `mydb`.`oeuvres` (`id_oeuvre`)
+  FOREIGN KEY (`auteur_id_auteurs`)
+  REFERENCES `mydb`.`auteurs` (`id_auteur`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
