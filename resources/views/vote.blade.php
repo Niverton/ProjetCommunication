27de18cc9719e6c1d577799fffc32fee5964b6ef
Timothée Jourde <itim.lcf@gmail.com>
2017-03-09 18:05:10 +0100

    design page de vote à peu près terminé

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -30,12 +30,18 @@
     
     @foreach ($artworks as $a)
     <div class="artwork" id="artwork{{$a['id']}}">
-      <img alt="photographie de l'œuvre {{$a['name']}}" src="{{$a['image']}}" />
-      <div class="info">
-        <h4 class="name">{{$a['name']}}</h4>
-        <time>{{$a['date']}}</time>
-        <span class="author">{{$a['author']}}</span>
-        <p class="description">{{$a['description']}}</p>
+      <a class="background" href="#n"></a>
+      <div>
+        <img alt="photographie de l'œuvre {{$a['name']}}" src="{{$a['image']}}" />
+        <div class="side">
+          <div class="info">
+            <h4 class="name">{{$a['name']}}</h4>
+            <div><span class="author">{{$a['author']}}</span> <time>{{$a['date']}}</time></div>
+            <p class="description">{{$a['description']}}</p>
+          </div>
+          <button>voter pour cette œuvre</button>
+          <a class="close" href="#n">retour à la gallerie</a>
+        </div>
       </div>
     </div>
     @endforeach      
