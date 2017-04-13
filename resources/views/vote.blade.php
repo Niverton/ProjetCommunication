dfb6da2702f6b44f957c155bdb1fc0e84f512d20
sammehdaoui <mehdaoui.o@gmail.com>
2017-04-13 14:45:14 +0200

    Update vote.blade.php

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -39,12 +39,14 @@
             <div><span class="author">{{$a['author']}}</span> <time>{{$a['date']}}</time></div>
             <p class="description">{{$a['description']}}</p>
           </div>
-          <button>Voter pour cette œuvre</button>
+          <button id="button{{$a['id']}}" onclick="voteCounter(this.id)">Voter pour cette œuvre</button>
+          <div id="votes"></div>
           <a class="close" href="#n">Retour à la galerie</a>
         </div>
       </div>
     </div>
-    @endforeach      
+    @endforeach  
+    <script type="text/javascript" src="/static/vote.js"></script>
     
   </body>
 
